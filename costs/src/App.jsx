5c00c01df802c1f30120c337fd575406520c2f77
Mateus Montenegro <mateus.montengro.20@gmail.com>
2022-04-12 20:48:22 -0300

    avanços no projetos

--- costs/src/App.jsx
+++ costs/src/App.jsx
@@ -1,8 +1,33 @@
 import './App.css';
+import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom'
+import Home from './components/pages/Home';
+import Contato from './components/pages/Contato';
+import Company from './components/pages/Company';
+import NewProject from './components/pages/NewProject';
+
+
+import Container from './components/layouts/Container';
 
 function App() {
   return (
-   <h1>costs</h1>
+      <Router>
+        <div>
+          <Link to='/'> Home</Link>
+          <Link to='/Contato'> Contact</Link>
+          <Link to='/Company'> Company</Link>
+          <Link to='/NewProject'> NewProject</Link>
+        </div>
+          <Container customClass="min-height"> {/* Container fora dos Routes */ }
+          <Routes>
+              <Route exact path="/" element={<Home/>}/> {/* Para a pagina inicial colocamos apenas / com  o "exact"*/}
+              <Route path="Company" element={<Company/>}/>
+              <Route path="Contato" element={<Contato/>}/>
+              <Route path='NewProject' element={ <NewProject />}/>
+          </Routes>
+          </Container>
+          <p>Footer</p>
+         
+      </Router>
   )
 }
 
