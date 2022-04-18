b7f9025b06fade4e4394b12e17ba0b92901f1c39
Mateus Montenegro <mateus.montengro.20@gmail.com>
2022-04-18 13:41:23 -0300

    mudanças no projeto

--- costs/src/components/pages/NewProject.js
+++ costs/src/components/pages/NewProject.js
@@ -1,11 +1,34 @@
 import styles from './NewProject.module.css'
+import  { useNavigate} from 'react-router-dom'
 import ProjectForm from '../projects/ProjectForm';
 const NewProject = () => {
+
+    const history = history()
+
+    function createPost(project){
+
+        //initialize cost and services
+        project.cost = 0
+        project.services = []
+
+        fetch('http:/localhost:5000/projects',{
+            method:'POST',
+            headers: {
+                'Content-Type' : 'aplicattion/json',
+            },
+            body: JSON.stringify(project)
+        }).then((resp => resp.json())
+        .then((data)=>{
+            console.log(data)
+            //redirect
+        })
+        .catch(err => console.log(err)),
+        )}
     return ( 
         <div className={styles.newproject_container}>
             <h1> Criar Projeto</h1>
             <p> Crie seus projetos para depois adicionar seus serviços</p>
-            <ProjectForm btntext='Criar projeto'/>
+            <ProjectForm btntext='Criar projeto' handleSubimit={createPost}/>
         </div>
      );
 }
